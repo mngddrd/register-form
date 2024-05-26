2fb9b17c2569400720b186f4cacbcf748897a339
adem <adembilalbilal@gmail.com>
2024-05-26 15:56:20 +0300

    register form

--- contact-form-main/2.js
+++
@@ -1,9 +0,0 @@
-
-
-
-
-console.log(digitalRoot(40))
-
-
-
-
--- contact-form-main/contact.js
+++ contact-form-main/contact.js
@@ -1,22 +1,36 @@
 const firstNameCon = document.querySelector(".name");
-const firstName = document.getElementById("First-Name")
+const firstName = document.getElementById("First-Name");
 
 const lastNameCont = document.querySelector(".last-name");
-const lastName = document.getElementById("last-Name2")
+const lastName = document.getElementById("last-Name2");
 
 const emailCont = document.querySelector(".second-section");
-const emailInput = document.getElementById("email")
+const emailInput = document.getElementById("email");
 
-const messContiner = document.querySelector(".fouth-section")
-const Message = document.getElementById("Message")
+const messContiner = document.querySelector(".fouth-section");
+const Message = document.getElementById("Message");
+
+const checkpar = document.querySelector(".ch-all");
+const checkbox  = document.getElementById("checkbox");
+
+const radiocontainer = document.querySelector(".third-section");
+const firstradio = document.getElementById("unique1");
+const firstcontai = document.querySelector(".query1");
+const secondradio = document.getElementById("unique");
+const secondcontai = document.querySelector(".query2");
+
 
 const butt = document.getElementById("butt");
 let elementCreated = false;
 let elementCreated2 = false;
 let elementCreated3 = false;
 let elementCreated4 = false;
+let elementCreated5 = false;
+let elementCreated6 = false;
 
 butt.addEventListener("click",function(e){
+
+  e.preventDefault()
   let inuptOne = firstName.value;
   let p1 = document.createElement("p")
   p1.className = "p1"
@@ -36,6 +50,14 @@ butt.addEventListener("click",function(e){
   let p4 = document.createElement("p");
   p4.className = "p1"
   p4.innerHTML = "Please enter a valid email address"
+
+  let p5 = document.createElement("p");
+  p5.className = "p1";
+  p5.innerHTML = "To submit this form please consent to being contacted"
+
+  let p6 = document.createElement("p");
+  p6.className = "p1";
+  p6.innerHTML = "Please select aquery Type"
 
   if(inuptOne === ""){
     if(!elementCreated){
@@ -72,8 +94,8 @@ butt.addEventListener("click",function(e){
     Message.style.border = "0.5px solid"
     messContiner.removeChild(messContiner.children[3])
   }
-
-  if(emailInp === ""){
+  console.log(emailInp)
+  if(  !( /@/.test(emailInp))){
     if(!elementCreated4){
       emailCont.appendChild(p4)
       elementCreated4 = true;
@@ -82,7 +104,36 @@ butt.addEventListener("click",function(e){
   }
   else{
     emailInput.style.border = "0.5px solid"
-    emailCont.removeChild(messContiner.children[3])
+    emailCont.removeChild(emailCont.children[4])
   }
+
+    let checked1 = checkbox.checked
+    console.log('Checkbox checked state:', checked1);
+  if(!checked1){
+    if(!elementCreated5){
+      checkpar.appendChild(p5)
+      elementCreated5 = true;
+    }
+  }
+  else{
+    checkpar.removeChild(checkpar.children[1])
+  }
+
+  if(!firstradio.checked && !secondradio.checked){
+    if(!elementCreated6){
+      radiocontainer.appendChild(p6)
+      elementCreated6 = true;
+    }
+  }
+  else{
+    radiocontainer.removeChild(radiocontainer.children[3])
+    if(firstradio.checked){
+      firstcontai.style.backgroundColor = "rgb(223, 241, 231)";
+    }
+    else{
+      secondcontai.style.backgroundColor = "rgb(223, 241, 231)";
+    }
+  }
+
   
 })
